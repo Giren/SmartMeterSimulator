f551ff9e90e4998018df98e6606469f66564a915
Michi <giren@web.de>
2015-05-07 17:14:48 +0200

    charts included

--- src/de/simulator/client/Simulator.java
+++ src/de/simulator/client/Simulator.java
@@ -1,5 +1,7 @@
 package de.simulator.client;
 
+import org.moxieapps.gwt.highcharts.client.*;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -7,8 +9,17 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class Simulator implements EntryPoint {
     Label title = new Label("Simulator");
 
-	public void onModuleLoad() {
-		RootPanel.get().add(title);
+	public void onModuleLoad() {				
+		Chart chart = new Chart()
+		   .setType(Series.Type.SPLINE)
+		   .setChartTitleText("Lawn Tunnels")
+		   .setMarginRight(10);
+		
+		Series series = chart.createSeries()
+				   .setName("Moles per Yard")
+				   .setPoints(new Number[] { 163, 203, 276, 408, 547, 729, 628 });
+				chart.addSeries(series);
+		
+		RootPanel.get().add(chart);
 	}
-
 }
